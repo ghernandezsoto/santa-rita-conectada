38ab937f6a8f959632f30e379145ecdaf86c6b8c
Gonzalo Hernández <ghernandez19@alumnos.utalca.cl>
2025-09-22 14:27:21 -0300

    modificando temporalmente para pruebas email

--- app/Notifications/NuevoComunicadoNotification.php
+++ app/Notifications/NuevoComunicadoNotification.php
@@ -10,7 +10,8 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Log; // <-- AÑADIR
 use Throwable; // <-- AÑADIR
 
-class NuevoComunicadoNotification extends Notification implements ShouldQueue
+#class NuevoComunicadoNotification extends Notification implements ShouldQueue
+class NuevoComunicadoNotification extends Notification
 {
     use Queueable;
 
