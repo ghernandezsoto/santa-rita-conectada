f591d0aa53080fcc0c10a0695cb8550dffccb84b
Gonzalo Hernández <ghernandez19@alumnos.utalca.cl>
2025-11-27 21:58:48 -0300

    cambios varios

--- app/Http/Controllers/Api/ActaController.php
+++ app/Http/Controllers/Api/ActaController.php
@@ -7,6 +7,7 @@ use App\Models\Acta;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 
+#aa
 class ActaController extends Controller
 {
     public function index()
--- app/Http/Controllers/Api/DocumentoController.php
+++ app/Http/Controllers/Api/DocumentoController.php
@@ -7,6 +7,7 @@ use App\Models\Documento;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 
+#aaaa
 class DocumentoController extends Controller
 {
     public function index()
